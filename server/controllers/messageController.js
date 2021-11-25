4ef8f0b73b2957c6e5a8dd752b6edc9917a23d50
Nadhif Rafifaiz K <nadhifrafifaiz@gmail.com>
2021-11-25 22:49:51 +0700

    read status added

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,13 +1,29 @@
 const { Conversations, Users, Messages } = require("../models/index")
+const { Op } = require("sequelize");
+
 
 module.exports = {
     AddMessage: async (req, res) => {
         try {
+            console.log(req.body.sender);
 
             // Add to database
             await Messages.create({
-                ...req.body
+                ...req.body, readStatus: false
             })
+
+            //Update Read Status
+            await Messages.update(
+                {
+                    readStatus: true
+                },
+                {
+                    where: {
+                        sender: { [Op.not]: req.body.sender }
+                    }
+                }
+
+            )
 
             // Send back all conversation
             const getMessages = await Messages.findAll({
@@ -15,10 +31,7 @@ module.exports = {
                     ConversationId: req.body.ConversationId
                 }
             })
-
             return res.status(200).json(getMessages)
-
-
 
 
         } catch (error) {
